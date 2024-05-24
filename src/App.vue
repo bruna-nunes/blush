63e6f1a906467b704c1597ceeea60fda6a8bd146
bruna-nunes <brugnunes@gmail.com>
2024-05-24 19:08:29 -0300

    feat: add blush button initial version

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,31 @@
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
     <comp-example msg="Componente exemplo" />
+    <blush-button
+      id="button1"
+      name="nameButton1"
+      type="button"
+      label="Button"
+      variant="primary"
+      size="large"
+      :disabled="false"
+      @onClick="clickButtonHandler"
+    />
   </div>
   
 </template>
 
 <script setup>
-  import CompExample from './components/CompExample.vue'
+import CompExample from './components/CompExample.vue'
+import blushButton from './components/blush-button/blush-button.vue';
+
+function clickButtonHandler(event) {
+  console.log(event)
+}
 </script>
 
 <style lang="scss">
 .content-app {
-  background-color: map-get($primary-absolute, '001');
-  text-align: center;
   padding: spacing(4);
 }
 </style>
