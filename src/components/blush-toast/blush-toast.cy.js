e0b1365f4012e352987a2d2f1694490886b36b59
bruna-nunes <brugnunes@gmail.com>
2024-11-21 22:51:10 -0300

    feat: add blush-toasts component

---
+++ src/components/blush-toast/blush-toast.cy.js
@@ -0,0 +1,35 @@
+import BlushToast from './blush-toast.vue'
+
+const props = {
+  title: 'Título do Toast',
+  text: 'Texto do toast',
+  type: 'informative',
+  showDismiss: false
+}
+
+describe('renders', () => {
+  it('render blush toast correctly based on provided props', () => {
+    cy.mount(BlushToast, { props })
+
+    cy.get('.blush-toast')
+      .should('have.css', 'background-color', 'rgb(242, 242, 242)')
+      .should('have.css', 'border-radius', '4px')
+      .should('have.css', 'padding', '12px')
+  })
+})
+
+describe('events', () => {
+  it('emit onDismiss event', () => {
+    cy.mount(BlushToast, { props:
+      {
+        ...props,
+        showDismiss: true,
+      },  
+    })
+
+    cy.get('.blush-toast img.dismiss-icon').click()
+      .then(() => {
+        cy.wrap(Cypress.vueWrapper.emitted('onDismiss')).should('have.length', 1)
+      })
+  })
+})
---
+++ src/plugins/toasts.js
@@ -0,0 +1,43 @@
+import { ref } from 'vue'
+
+const toastsQueue = ref([])
+
+export default {
+  install: (app) => {
+
+    const add = (toastData, timeout = 4800) => {
+      toastData = Object.assign({ id: Date.now() }, toastData)
+      toastsQueue.value.push(toastData)
+
+      if (timeout) {
+        setTimeout(() => {
+          remove(toastData.id)
+        }, timeout)
+      }
+
+      return toastData.id
+    }
+
+    const remove = (id) => {
+      return toastsQueue.value.splice(
+        toastsQueue.value.findIndex((n) => n.id === id),
+        1
+      )
+    }
+
+    const removeAll = () => {
+        toastsQueue.value.length = 0
+    }
+
+    const toastsApi = {
+      queue: toastsQueue.value,
+      add,
+      remove,
+      removeAll
+    }
+
+    // Provides Toasts API for external usage
+    app.config.globalProperties.$toasts = toastsApi
+    app.provide('toasts', toastsApi)
+  }
+}
